29af4e464ff85e206354f448b4b097ad9b2415ef
Rommel05 <rrll20220@gmail.com>
2024-03-26 10:31:08 +0100

    proyecto 2

--- src/main/java/MusicaRepositoryImpI.java
+++ src/main/java/MusicaRepositoryImpI.java
@@ -3,8 +3,8 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
-public class MusicaRepositoryImpI{
-    private static Set<Cantante> musicaCached = new HashSet<>();
+public class MusicaRepositoryImpI {
+    private static Set<Cantante> cantanteCached = new HashSet<>();
     private java.sql.Connection con;
     public MusicaRepositoryImpI() throws SQLException{
         this.con = MusicaService.getConnection();
