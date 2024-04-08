4d07e38916dc03dfdf734c06a311b7520f1cc5f1
Rommel05 <rrll20220@gmail.com>
2024-04-08 10:40:01 +0200

    proyecto 2

--- src/main/java/MusicController.java
+++ src/main/java/MusicController.java
@@ -1,2 +1,17 @@
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
 public class MusicController {
+    private static final MusicaRepositoryImpl musicRepository = new MusicaRepositoryImpl();
+    public static void prntAllSingers() throws SQLException {
+        List<Cantante> cantantes = musicRepository.findAll();
+        for (Cantante cantante : cantantes) {
+            System.out.println(cantante);
+        }
+    }
+
+    public static void findbyid() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+    }
 }
