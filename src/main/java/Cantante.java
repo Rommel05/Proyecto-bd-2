ca45d3c0d443827688c3f9e1463ec459a922b911
Rommel05 <rrll20220@gmail.com>
2024-04-08 09:37:41 +0200

    proyecto 2

--- src/main/java/Cantante.java
+++ src/main/java/Cantante.java
@@ -1,14 +1,24 @@
 public class Cantante {
     private int id;
     private String nombre;
+    private int edad;
+    private String nacionalidad;
+    private String generoMusical;
 
-    public Cantante(int id, String nombre) {
+    public Cantante(int id, String nombre, int edad, String nacionalidad, String generoMusical) {
         this.id = id;
         this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.generoMusical = generoMusical;
     }
 
-    public Cantante(String nombre) {
-        this(-1,nombre);
+    public Cantante(String nombre, int edad, String nacionalidad, String generoMusical) {
+        this.id = -1;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.generoMusical = generoMusical;
     }
 
     public int getId() {
@@ -25,6 +35,30 @@ public class Cantante {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getGeneroMusical() {
+        return generoMusical;
+    }
+
+    public void setGeneroMusical(String generoMusical) {
+        this.generoMusical = generoMusical;
     }
 
     @Override
---
+++ src/main/java/MusicaRepositoryImpl.java
@@ -0,0 +1,81 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicaRepositoryImpl implements IRepository<Cantante> {
+    private java.sql.Connection con;
+    public MusicaRepositoryImpl() throws SQLException{
+        this.con = MusicaService.getConnection();
+    }
+    public Cantante bdToEntity(ResultSet rs) throws SQLException {
+        return new Cantante(rs.getInt("id"), rs.getNString("nombre"), rs.getInt("edad"),rs.getNString("nacionalidad"), rs.getNString("generoMusical"));
+    }
+
+    public void save(Cantante cantante) throws SQLException{
+        ResultSet rs;
+        PreparedStatement st = null;
+        if (cantante.getId() == -1) {
+            String query = "INSERT INTO artistas (nombre, edad, nacionalidad, generoMusical) VALUES (?, ?, ?, ?)";
+
+            st = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, cantante.getNombre());
+            st.setInt(2,cantante.getEdad());
+            st.setString(3,cantante.getNacionalidad());
+            st.setString(4, cantante.getNacionalidad());
+
+            st.executeUpdate();
+
+            //Recuperar el id autogenerado
+            rs = st.getGeneratedKeys();
+            //Este ResultSet solo puede contener un registro: el ID autogenerado
+
+            if (rs.next()){
+                //Ahora ya sabemos cuál es el nuevo id del Usuario
+                cantante.setId(rs.getInt(1));
+            }
+
+        }
+
+    }
+
+    public void delete(Cantante cantante) throws SQLException{
+        PreparedStatement st = con.prepareStatement("DELETE FROM artistas WHERE id = ?");
+        st.setInt(1, cantante.getId());
+        st.executeUpdate();
+        st.close();
+    }
+
+    public List<Cantante> findAll() throws SQLException {
+
+        List<Cantante> cantantes = new ArrayList<>();
+
+        Statement st = this.con.createStatement();
+        //Ejecutar la consulta, guardando los datos devueltos en un Resulset
+        ResultSet rs = st.executeQuery("SELECT * FROM artistas");
+
+        while(rs.next()){
+            //Mapeamos el registro de la BD en un post
+            Cantante cantante = bdToEntity(rs);
+            //Añadir el Post al conjunto de posts
+            cantantes.add(cantante);
+
+        }
+        return cantantes;
+    }
+
+    public Cantante findById(int id) throws SQLException {
+        Cantante cantante = null;
+        PreparedStatement st = con.prepareStatement("SELECT nombre, edad, nacionalidad, generoMusical FROM artistas WHERE id = ?");
+        st.setInt(1,id);
+        ResultSet rs = st.executeQuery();
+
+        if(rs.next()) {
+            cantante = bdToEntity(rs);
+        }
+        return cantante;
+    }
+}
